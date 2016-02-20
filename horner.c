a5c4fe4e169ceb67b38c428b2ba2660721427520
achim-o <achim.oetringhaus@gmx.net>
2016-02-20 11:55:04 +0100

    horner polynomgrad

--- horner.c
+++ horner.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/*
+ * horner:
+ * x: die zu untersuchende Stelle
+ * n: Grad des Polynoms (höchste vorkommende Potenz)
+ * a: Vektor der Koeffizienten. Die Länge ist um 1 größer als der Grad!
+ */
 double horner (double x, int n, double * a)
 {
 	double y = 0;
@@ -12,13 +18,19 @@ int main (void)
 	double coeff [] = { 36, -13, 1 };  /* x^2 - 13 x + 36 */
 	int n_coeff = sizeof (coeff) / sizeof (coeff[0]);
 	
+	/*
+	 * Achtung: n_coeff ist um 1 höher als der Grad des Polynoms!
+	 * Das wird in der Variablen grade berücksichtigt.
+	 */
+	 int grade = n_coeff - 1;
+	
 	/* Wertetabelle, x = 0 .. 9 */
 	int i;
 	for (i = 0; i < 10; i++)
 	{
 		/* Funktion horner aufrufen und Ergebnis speichern */
 		double x = i;
-		double y = horner (x, n_coeff, coeff);
+		double y = horner (x, grade, coeff);
 		
 		/* Tabelle ausgeben */
 		printf ("x = %lf | y = %lf\n", x, y);
